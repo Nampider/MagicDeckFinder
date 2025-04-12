93d224a8481ce1d2890d912ab295d2c61162c0b2
Nampider <devchrisnam@gmail.com>
2025-04-12 11:03:40 -0700

    basic webscraping work

--- ai_python/develop.py
+++ ai_python/develop.py
@@ -1,56 +1,70 @@
-#This is an example file for webscraping
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By #use this to search for elements in html
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait #for waiing for web to load fully.
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-#setup the chrome driver service
-#we have to create a webdriver. Automation tool to control google chrome. Chrome is the most populat
+# Setup Chrome driver
+options = Options()
+options.add_argument("--start-maximized")
 
-service = Service(executable_path="./chromedriver")
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://darksidegames.com/")
+def web_scrape(driver: webdriver, search_term: str = "Sol Ring"):
+    driver.get("https://darksidegames.com/")
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "search-bar__input"))
+    )
 
-def web_scrape(driver:webdriver):
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "search-bar__input")))  #basically the above code says to wait until you can find the class with gLFyf
-
-    input_element = driver.find_element(By.CLASS_NAME, "search-bar__input")  #look for first element on the page with this class and return it.
+    input_element = driver.find_element(By.CLASS_NAME, "search-bar__input")
     input_element.clear()
-    input_element.send_keys("The Mouth of Sauron" + Keys.ENTER)
+    input_element.send_keys(search_term + Keys.ENTER)
 
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "The Mouth of Sauron"))) 
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "productCard__title"))
+    )
 
-    card_price = driver.find_elements(By.CLASS_NAME, "productCard__price")
-    card_title = driver.find_elements(By.CLASS_NAME, "productCard__title ")
-    card_stock = driver.find_elements(By.CLASS_NAME, "productChip__active")
-    card_set = driver.find_elements(By.CLASS_NAME, "productCard__setName")
     card_info = []
-    for i,j,k,l in zip(card_price, card_title, card_stock, card_set):
-        if j.text == "The Mouth of Sauron":
-            card_info.append((i.text, j.text, l.text, k.text))
+    
+    while True:
+        time.sleep(1)  # Helps if the page is JS-heavy
 
-    print("Lets go through the card_information list that I gathered")
-    for f in card_info:
-        print(f)
+        prices = driver.find_elements(By.CLASS_NAME, "productCard__price")
+        titles = driver.find_elements(By.CLASS_NAME, "productCard__title")
+        stocks = driver.find_elements(By.CLASS_NAME, "productChip__active")
+        sets = driver.find_elements(By.CLASS_NAME, "productCard__setName")
 
-web_scrape(driver)
+        for price, title, stock, set_name in zip(prices, titles, stocks, sets):
+            if title.text.strip().lower() == search_term.strip().lower():
+                card_info.append({
+                    "title": title.text,
+                    "price": price.text,
+                    "stock": stock.text,
+                    "set": set_name.text
+                })
 
+        # Try to move to the next page if it exists
+        try:
+            next_button = WebDriverWait(driver, 3).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "pagination__next"))
+            )
+            if 'disabled' in next_button.get_attribute('class'):
+                break
+            next_button.click()
+        except:
+            break  # No more pages
 
+    return card_info
 
-# link = driver.find_element(By.PARTIAL_LINK_TEXT, "Tech With Tim")   #find element is first thing on page. find_elements with a s returns an array of everything.
-# link.click()
+# Call the function and print results
+results = web_scrape(driver, "Sol Ring")
+
+print("\nCollected card data:")
+for card in results:
+    print(card)
 
 driver.quit()
-
-""""
-These are the classes I will need from darkside games.
- productCard__price
-productCard__title -> href\
-productCard__img
-productChip__active 
-"""
